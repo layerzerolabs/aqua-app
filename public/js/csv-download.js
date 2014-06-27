ef306ff9754d003f9d9637cdf7316efec6f71c1c
naomi <naomi@l0l.org.uk>
2014-06-27 16:43:01 +0100

    Working but not date range.

--- public/js/csv-download.js
+++ public/js/csv-download.js
@@ -2,35 +2,5 @@ $(function(){
 	$("#from, #to").datepicker({
 		dateFormat: "d/m/yy"
 	});
-	$("#download_form").submit(function(event) {
-		event.preventDefault();
-		var form_data = $(this).serializeArray();
-		var url = "http://localhost:8001/todmorden/";
-    var sensor_name = form_data[2].value;
-		if (sensor_name != "all") {
-			url = url + sensor_name;
-		}
-		$.ajax({
-			 url: url,
-			 method: 'get',
-			 success:function(json){
-					console.log(json);	
-					download(convert_to_csv(json));
-			 },
-			 error:function(){
-			    alert("Error getting data");
-			 }      		
-		});
-	});
 });
 
-function convert_to_csv(json) {
-	var csv = "'sensor name', 'date/time', 'reading'\n";
-	$.each(json.rows, function(i, row){
-		csv += "'"+row.sensor_name+"', '"+row.reading_time+"', '"+row.reading_value+"'\n";
-	});
-	alert(csv);
-	return csv;
-}
-
-function download(csv){}
--- server.js
+++ server.js
@@ -11,6 +11,42 @@ app.use(allowCrossDomain);
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/csv_download', function(req, res){
+	var url = require('url');
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	var api_url = "http://localhost:8001/todmorden";
+	if (query.sensor_name != 'all') {
+		api_url += '/'+query.sensor_name;
+	}	
+	var request = require('request');
+	request.get(api_url, function (error, api_res, body) {
+		console.log("statusCode: ", api_res.statusCode);
+		console.log(body);
+		if (!error && api_res.statusCode == 200) {
+			res.setHeader('Content-disposition', 'attachment; filename=todmorden-'+query.sensor_name+'.csv');
+			res.setHeader('Content-type', 'csv');
+			res.charset = 'UTF-8';
+			res.write(convert_to_csv(body));
+			res.end();
+		}
+	});
+});
+
+function convert_to_csv(body) {
+	var json = JSON.parse(body);
+	var csv = "sensor name, date/time, reading\n";
+	for (var i = 0; i < json.rows.length; i++){
+		var row = json.rows[i];
+		csv += row.sensor_name+", "+date_format(row.reading_time)+", "+row.reading_value.toFixed(2)+"\n";
+	}
+	return csv;
+}
+
+function date_format(timestamp) {
+	return timestamp.substring(0,19).replace("T", " ");
+}
+
 app.listen(8080);
 console.log("Server running at http://127.0.0.1:8080/");
 
